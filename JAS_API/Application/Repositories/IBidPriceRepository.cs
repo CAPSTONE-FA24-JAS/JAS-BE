f34cd69100021ae24433b7b9cc4af66d5ff66006
Duong Thi Hien <134600777+DuongHienne@users.noreply.github.com>
2024-10-23 00:45:41 +0700

    MyBid/Add_query_yourMaxBidPrice_and_Change_ResponseCustomerLot

--- JAS_API/Application/Repositories/IBidPriceRepository.cs
+++ JAS_API/Application/Repositories/IBidPriceRepository.cs
@@ -9,5 +9,6 @@ namespace Application.Repositories
 {
     public interface IBidPriceRepository : IGenericRepository<BidPrice>
     {
+        Task<BidPrice> GetMaxBidPriceByCustomerIdAndLot(int? customerId, int? lotId);
     }
 }
--- JAS_API/Infrastructures/Repositories/LotRepository.cs
+++ JAS_API/Infrastructures/Repositories/LotRepository.cs
@@ -23,60 +23,6 @@ namespace Infrastructures.Repositories
             _dbContext = context;
         }
 
-        public async Task<(IEnumerable<Lot> data, int totalItems)> GetBidsOfCustomer(int? customerIId, string? status, int? pageIndex, int? pageSize)
-        {
-            var customerLots = _dbContext.Lots.Include(x => x.CustomerLots)
-                                               .Where(x => x.CustomerLots.Any(cs => cs.CustomerId == customerIId && cs.Status == status));
-            if (pageIndex.HasValue && pageSize.HasValue)
-            {
-                int validPageIndex = pageIndex.Value > 0 ? pageIndex.Value - 1 : 0;
-                int validPageSize = pageSize.Value > 0 ? pageSize.Value : 10; // Assuming a default pageSize of 10 if an invalid value is passed
-
-                customerLots = customerLots.Skip(validPageIndex * validPageSize).Take(validPageSize);
-            }
-
-            var products = await customerLots.ToListAsync();
-
-            var totalItems = products.Count;
-
-            if (products != null && products.Any())
-            {
-                return (products, totalItems);
-            }
-            else
-            {
-                throw new Exception("Don't have any Lots");
-            }
-        }
-
-
-        public async Task<(IEnumerable<Lot> data, int totalItems)> GetPastBidOfCustomer(int customerIId, IEnumerable<string> status, int? pageIndex, int? pageSize)
-        {
-            var lots = _dbContext.Lots.Include(x => x.CustomerLots)
-                                      .Where(x => x.CustomerLots.Any(cs => status.Contains(cs.Status) && cs.CustomerId == customerIId));
-
-            if (pageIndex.HasValue && pageSize.HasValue)
-            {
-                int validPageIndex = pageIndex.Value > 0 ? pageIndex.Value - 1 : 0;
-                int validPageSize = pageSize.Value > 0 ? pageSize.Value : 10; // Assuming a default pageSize of 10 if an invalid value is passed
-
-                lots = lots.Skip(validPageIndex * validPageSize).Take(validPageSize);
-            }
-
-            var products = await lots.ToListAsync();
-
-            var totalItems = products.Count;
-
-            if (products != null && products.Any())
-            {
-                return (products, totalItems);
-            }
-            else
-            {
-                throw new Exception("Don't have any Lots");
-            }
-
-
-        }
+        
     }
 }
