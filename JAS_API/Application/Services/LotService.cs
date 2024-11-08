645f3c10ff8bdebab6f87cf1c0c9418025e928de
congdanh1z9 <congdangdanh@gmail.com>
2024-11-08 17:02:32 +0000

    fix check wallet when bidding

--- JAS_API/Application/Services/LotService.cs
+++ JAS_API/Application/Services/LotService.cs
@@ -657,6 +657,15 @@ namespace Application.Services
                 if (lot != null)
                 {
 
+                    var customer = await _unitOfWork.CustomerRepository.GetByIdAsync(model.CustomerId);
+                    if(customer != null && customer.Wallet.AvailableBalance < (decimal?)model.CurrentPrice)
+                    {
+                        response.Code = 400;
+                        response.IsSuccess = false;
+                        response.Message = $"Customer dont enought money available with current price for bid.";
+                        return response;
+
+                    }
                     if (model.CurrentPrice < lot.StartPrice || model.CurrentPrice > lot.FinalPriceSold && lot.LotType == EnumLotType.Secret_Auction.ToString())
                     {
                         response.Code = 400;
