624408c4b62b513303840b1972bca410e19e2a3c
Đặng Công Danh <104542816+congdanh1z9@users.noreply.github.com>
2024-11-12 17:09:00 +0000

    Update Program.cs

--- JAS_API/WebAPI/Program.cs
+++ JAS_API/WebAPI/Program.cs
@@ -65,6 +65,7 @@ builder.Services.AddSingleton<LiveBiddingService>();
 builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 
 //dki background service
+builder.Services.AddHostedService<ProcessStreamService>();
 builder.Services.AddHostedService<AuctionMonitorService>();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Services.AddEndpointsApiExplorer();
