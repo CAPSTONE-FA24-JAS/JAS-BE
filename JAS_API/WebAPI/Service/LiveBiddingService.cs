d3c693997640409ca8de219fcb209057142bd3b9
congdanh1z9 <congdangdanh@gmail.com>
2024-11-17 19:25:58 +0000

    fix

--- JAS_API/WebAPI/Service/LiveBiddingService.cs
+++ JAS_API/WebAPI/Service/LiveBiddingService.cs
@@ -640,7 +640,10 @@ namespace WebAPI.Service
                         else
                         {
                             lot.Status = EnumStatusLot.Passed.ToString();
-                            var losers = lot.CustomerLots.Where(x => x.CustomerId != bidPriceWinner.CustomerId).ToList();
+                            var losers = lot.CustomerLots
+                                         .Where(x => x.CustomerId != (bidPriceWinner?.CustomerId ?? -1))
+                                         .ToList();
+
                             await SetLoser(losers);
                         }
 
@@ -651,8 +654,6 @@ namespace WebAPI.Service
                         await _unitOfWork.SaveChangeAsync();
                     }
                 }
-
-
             }
         }
         public async Task CheckLotSercetAsync()
