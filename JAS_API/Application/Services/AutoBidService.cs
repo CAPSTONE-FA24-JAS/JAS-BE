bb4e7c1b443d4ad23480234a7169442fb9232d03
congdanh1z9 <congdangdanh@gmail.com>
2024-11-22 09:42:33 +0000

    fix check  min max when create autobid

--- JAS_API/Application/Services/AutoBidService.cs
+++ JAS_API/Application/Services/AutoBidService.cs
@@ -24,32 +24,41 @@ namespace Application.Services
             try
             {
 
-                var autoBid = _mapper.Map<AutoBid>(createAutoBidDTO);
-                autoBid.IsActive = true;
                 var customerLotOfAutoBid = await _unitOfWork.CustomerLotRepository.GetByIdAsync(createAutoBidDTO.CustomerLotId);
-                customerLotOfAutoBid.IsAutoBid = true;
-                if (autoBid == null)
+                if (customerLotOfAutoBid.AutoBids.Any(x => (x.MinPrice < createAutoBidDTO.MinPrice && x.MaxPrice > createAutoBidDTO.MaxPrice) ||
+                                                          (x.MinPrice < createAutoBidDTO.MaxPrice && x.MaxPrice > createAutoBidDTO.MaxPrice)))
                 {
+                    response.Message = $"Set New AutoBid Faid, Autobid of player must out range old autobid";
+                    response.Code = 400;
                     response.IsSuccess = false;
-                    response.Message = "Mapper failed";
                 }
                 else
                 {
-                    _unitOfWork.AutoBidRepository.AddAsync(autoBid);
-                    if (await _unitOfWork.SaveChangeAsync() > 0)
+                    var autoBid = _mapper.Map<AutoBid>(createAutoBidDTO);
+                    autoBid.IsActive = true;
+                    customerLotOfAutoBid.IsAutoBid = true;
+                    if (autoBid == null)
                     {
-                        response.Message = $"Set New AutoBid Successfully";
-                        response.Code = 200;
-                        response.IsSuccess = true;
+                        response.IsSuccess = false;
+                        response.Message = "Mapper failed";
                     }
                     else
                     {
-                        response.Message = $"Save DB failed!";
-                        response.Code = 400;
-                        response.IsSuccess = false;
+                        _unitOfWork.AutoBidRepository.AddAsync(autoBid);
+                        if (await _unitOfWork.SaveChangeAsync() > 0)
+                        {
+                            response.Message = $"Set New AutoBid Successfully";
+                            response.Code = 200;
+                            response.IsSuccess = true;
+                        }
+                        else
+                        {
+                            response.Message = $"Save DB failed!";
+                            response.Code = 400;
+                            response.IsSuccess = false;
+                        }
                     }
                 }
-
             }
             catch (Exception ex)
             {
