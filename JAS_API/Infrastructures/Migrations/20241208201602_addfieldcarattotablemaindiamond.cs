0038002dd583cfe3e54b51669072b37fe6ec4e6d
congdanh1z9 <congdangdanh@gmail.com>
2024-12-08 20:18:01 +0000

    code first

---
+++ JAS_API/Infrastructures/Migrations/20241208201602_addfieldcarattotablemaindiamond.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfieldcarattotablemaindiamond : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Carat",
+                table: "MainDiamonds",
+                type: "real",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Carat",
+                table: "MainDiamonds");
+        }
+    }
+}
